2615d8f6784e84b99cd4ea3ba2f1d8afab41e9f5
Gabriel <ghpnet@gmail.com>
2022-11-25 00:00:19 -0300

    bd corrigido

--- log.py
+++ log.py
@@ -1,6 +1,6 @@
-import psycopg2 as pg
 class log:
     def novoLog():
+        import db
         novolog= 'INSERT INTO log (matricula,login,motivo) VALUES (matricula,login,"Inclusão de nota")'
         cursor.execute (novolog)
         print("Log com ação foi incluido")
