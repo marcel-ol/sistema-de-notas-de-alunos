67421759ae248348279181e5f7251ee115001e9c
Gabriel <ghpnet@gmail.com>
2022-11-24 23:34:58 -0300

    ..

--- db.py
+++ db.py
@@ -1,5 +1,3 @@
 import psycopg2 as pg
-def conexao():
-    # Conectar ao banco de dados. 
-    conexao = pg.connect(host='127.0.0.1', database='postgres', user='postgres', password='1234')
-    cursor=conexao.cursor()
+conexao = pg.connect(host='127.0.0.1', database='postgres', user='postgres', password='1234')
+cursor=conexao.cursor()
--- log.py
+++ log.py
@@ -1,6 +1,12 @@
 import psycopg2 as pg
 class log:
-    def conexao(self):
-        #from usuario import conexao
+    def novoLog(self,matricula,login,motivo):
+        #self.conexao()
         import db
-        
+        try:
+            novolog= 'INSERT INTO log (matricula,login,motivo) VALUES (%s,%s,%s)'
+            self.cursor.execute (novolog)
+            self.conexao.commit()
+        except Exception as e:
+            print ("Erro. Exceção: %s" % (e))
+            self.conexao.rollback()
