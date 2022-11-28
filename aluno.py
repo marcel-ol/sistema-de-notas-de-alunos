5f71fdbe37c4b9656861469124f5716ee5c20c48
Marcel <marcel.ol@outlook.com>
2022-11-27 23:39:38 -0300

    função incluir aluno e voltar funcionando

--- aluno.py
+++ aluno.py
@@ -1,89 +1,57 @@
-import psycopg2 as pg
-
 class Aluno:
-    conexao = pg.connect(host='127.0.0.1', database='postgres', user='postgres', password='1234')
-    cursor=conexao.cursor()
-
     def __init__(self):
-        print ("Controle de Aluno")
-        print("=================================")
-        print("1: Buscar Aluno")
-        print("2: Incluir novo aluno")
-        print("3: Alterar Aluno")
-        escolha = input("Escolha uma opção:")
-        if escolha == "1":
-            self.buscarAluno()
-        elif escolha == "2":
-            self.incluirAluno()
+        import psycopg2 as pg
+        self.conexao = pg.connect(host='127.0.0.1', database='postgres', user='postgres', password='1234')
+        self.cursor=self.conexao.cursor()
+        # print ("Controle de Aluno")
+        # print("=================================")
+        # print("1: Buscar Aluno")
+        # print("2: Incluir novo aluno")
+        # print("3: Alterar Aluno")
+        # escolha = input("Escolha uma opção:")
+        # if escolha == "1":
+        #     self.buscarAluno()
+        # elif escolha == "2":
+        #     self.incluirAluno()
         #self.incluirAluno()
 
-    def buscarAluno(self):
-        login = "admin"
-        print("\tBuscar Aluno")
-        print("=================================")
-        print("1: Buscar aluno pela matricula")
-        print("2: Buscar aluno pelo nome")
-        escolha = input("Escolha uma opção:")
-
-        if escolha == "1":
-            matricula = input("Qual a matricula do aluno:")
-            buscaNota = "SELECT matricula,nome,av1,av2,av3,av4 FROM aluno WHERE matricula = " +matricula+ " limit 1"
+    def buscarAluno(self,escolha,termo):
+        if escolha == 1:
+            # matricula = input("Qual a matricula do aluno:")
+            buscaNota = "SELECT matricula,nome,av1,av2,av3,av4 FROM aluno WHERE matricula = " +termo+ " limit 1"
             self.cursor.execute (buscaNota) #,matricula)
             resultado = self.cursor.fetchone()
-            print (resultado)
 
-        elif escolha == "2":
-            nome = input("Qual o nome do aluno:")
-            buscaNota = "SELECT matricula,nome,av1,av2,av3,av4 FROM aluno WHERE nome LIKE '%" +nome+ "%' limit 1"
+        elif escolha == 2:
+            # nome = input("Qual o nome do aluno:")
+            buscaNota = "SELECT matricula,nome,av1,av2,av3,av4 FROM aluno WHERE nome LIKE '%" +termo+ "%' limit 1"
             self.cursor.execute (buscaNota) #,matricula)
             resultado = self.cursor.fetchone()
-            print (resultado)
-        else:
-            print("Escolha um opção valida")
+        print (resultado)
+        return resultado
 
-    def incluirAluno(self):
-        login = "admin"
-        print("\n\n")
-        print("\tInserir novo aluno")
-        print("=================================")
-        print("Digite abaixo com atenção as notas do aluno")
-        matricula = input("Matricula do aluno?")
-        nome = input("Nome do aluno:")
-        av1 = input("Nota da AV1:")
-        av2 = input("Nota da AV2:")
-        av3 = input("Nota da AV3:")
-        av4 = input("Nota da AV4:")
+    def incluirAluno(self,idUsuario,nome,matricula,av1,av2,av3,av4):
+        motivo = None
+        try:
+            salvaNota = "INSERT INTO aluno (matricula,nome,av1,av2,av3,av4) VALUES (%s,%s,%s,%s,%s,%s)"
+            self.cursor.execute (salvaNota,(matricula,nome,av1,av2,av3,av4))
+            self.conexao.commit()
+            print(f"Aluno {nome} inserido.")
+            motivo = "Inclusão de aluno"
+        except Exception as e:
+            print ("Erro na inclusão. Exceção: %s" % (e))
+            self.conexao.rollback()
+            return 1
 
-        if (matricula == ""):
-            print("Digite a porra de uma matricula !")
-        elif(nome == ""):
-            print("Digite a desgraça do nome do aluno:")
-        elif(av1 == ""):
-            print ("Nota da AV1 é obrigatoria")
-        elif(av2 == ""):
-            print ("Nota da AV2 é obrigatoria")
-        elif(av3 == ""):
-            print ("Nota da AV3 é obrigatoria")
-        else:
-            try:
-                salvaNota = "INSERT INTO aluno (matricula,nome,av1,av2,av3,av4) VALUES (%s,%s,%s,%s,%s,%s)"
-                self.cursor.execute (salvaNota,(matricula,nome,av1,av2,av3,av4))
-                self.conexao.commit()
-                print(f"Nota do aluno {nome} foi inserida.")
-                aviso = "Inclusão de nota"
-            except Exception as e:
-                print ("Matricula ja wexiste. Exceção: %s" % (e))
-                self.conexao.rollback()
-
-            try:
-                novolog= "INSERT INTO log (matricula,login,motivo) VALUES (%s,%s,%s)"
-                self.cursor.execute (novolog,(matricula,'Admin',aviso))
-                self.conexao.commit()
-                print("Log com ação foi incluido")
-            except Exception as e:
-                print ("Erro encontrado. Exceção: %s" % (e))
-                self.conexao.rollback()
-
-#Primeira linha a ser executada
-obj = Aluno()
-# obj.mainloop()
+        try:
+            novolog= "INSERT INTO log (matricula,login,motivo) VALUES (%s,%s,%s)"
+            self.cursor.execute (novolog,(matricula,idUsuario,motivo))
+            self.conexao.commit()
+            print("Ação gravada no Log.")
+            if(motivo != None):
+                return -1
+            else: return 2
+        except Exception as e:
+            print ("Erro encontrado. Exceção: %s" % (e))
+            self.conexao.rollback()
+            return 3
--- interface.py
+++ interface.py
@@ -1,11 +1,11 @@
 import tkinter as tk
-# import psycopg2 as pg
-from usuario import *
 from tkinter import messagebox
+from usuario import *
+from aluno import *
 
 class Principal (tk.Frame):
     idUsuario = None
-
+    retornoAluno = None
     def __init__(self):
         self.telaLogin()
         self.master.geometry("1000x600")
@@ -13,8 +13,8 @@ class Principal (tk.Frame):
 
     def telaLogin(self):
         tk.Frame.__init__(self)
-        self.label1 = tk.Label(self.master,text="Login",font="Arial 18")
-        self.label1.grid(row=0,column=0,ipadx=20,ipady=20,padx=10,pady=10)
+        self.labelMenu = tk.Label(self.master,text="Login",font="Arial 18")
+        self.labelMenu.grid(row=0,column=0,ipadx=20,ipady=20,padx=10,pady=10)
         self.botaoEfetuarLogin = tk.Button(self.master,text="Login Usuário",font="Arial 14",command = lambda: self.iniciaLoginUsuario())
         self.botaoEfetuarLogin.grid(row=1,column=0,ipadx=20,ipady=20,padx=10,pady=10)
         self.botaoCriarLogin = tk.Button(self.master,text="Criar Usuário",font="Arial 14",command = lambda: self.iniciaCriarUsuario())
@@ -26,14 +26,14 @@ class Principal (tk.Frame):
         
     
     def telaMenu(self):
-        labelMenu = tk.Label(self.master,text="Menu",font="Arial 18")
-        labelMenu.grid(row=0,column=0,ipadx=20,ipady=20,padx=10,pady=10)
-        botaoIncluir = tk.Button(self.master,text="Incluir",font="Arial 14",command=self.incluir)
-        botaoIncluir.grid(row=1,column=0,ipadx=20,ipady=20,padx=10,pady=10)
-        botaobuscar = tk.Button(self.master,text="Buscar",font="Arial 14",command=self.buscar)
-        botaobuscar.grid(row=2,column=0,ipadx=20,ipady=20,padx=10,pady=10)
-        botaoSair = tk.Button(self.master,text="Sair",bg="red",fg="white",font="Arial 14 bold",command=self.sair)
-        botaoSair.grid(row=4,column=0,ipadx=20,ipady=20,padx=10,pady=20)
+        self.labelMenu = tk.Label(self.master,text="Menu",font="Arial 18")
+        self.labelMenu.grid(row=0,column=0,ipadx=20,ipady=20,padx=10,pady=10)
+        self.botaoIncluir = tk.Button(self.master,text="Incluir",font="Arial 14",command=self.telaIncluir)
+        self.botaoIncluir.grid(row=1,column=0,ipadx=20,ipady=20,padx=10,pady=10)
+        self.botaoBuscar = tk.Button(self.master,text="Buscar",font="Arial 14",command=self.buscar)
+        self.botaoBuscar.grid(row=2,column=0,ipadx=20,ipady=20,padx=10,pady=10)
+        self.botaoSair = tk.Button(self.master,text="Sair",bg="red",fg="white",font="Arial 14 bold",command=self.sair)
+        self.botaoSair.grid(row=4,column=0,ipadx=20,ipady=20,padx=10,pady=20)
 
     def iniciaLoginUsuario(self):
         print("iniciaLoginUsuario")
@@ -57,7 +57,6 @@ class Principal (tk.Frame):
         if (self.idUsuario == 0):
             tk.messagebox.showerror(title="Erro", message="Usuário/senha não conferem.")
         else:
-            print("login ok")
             self.labelTituloSecao.destroy()
             self.labelLogin.destroy()
             self.labelSenha.destroy()
@@ -67,7 +66,7 @@ class Principal (tk.Frame):
             self.botaoCriarLogin.destroy()
             self.botaoEfetuarLogin.destroy()
             self.botaoSair.destroy()
-            self.label1.destroy()
+            self.labelMenu.destroy()
             self.telaMenu()
 
     def iniciaCriarUsuario(self):
@@ -89,22 +88,96 @@ class Principal (tk.Frame):
 
     def executaCriarUsuario(self,login,senha):
         self.idUsuario = usuario.incluirUsuario (login,senha)
-        if (self.idUsuario == 0):
-            tk.messagebox.showerror(title="Erro", message="Erro ao criar usuário")
-        else:
+        if (self.idUsuario == -1):
             print("Usuário criado! Retorne e faça seu login.")
             print(self.idUsuario)
-            self.botaoCriarLogin["state"] = "enabled"
-            self.botaoEfetuarLogin["state"] = "enabled"
+            self.botaoCriarLogin["state"] = "active"
+            self.botaoEfetuarLogin["state"] = "active"
+            self.labelTituloSecao.destroy()
+            self.labelLogin.destroy()
+            self.labelSenha.destroy()
+            self.entradaLogin.destroy()
+            self.entradaSenha.destroy()
+        else:
+            tk.messagebox.showerror(title="Erro", message="Erro ao criar usuário")
 
     def buscar(self):
         print("buscar")
-    def incluir(self):
-        print("Incluir")
+    def telaIncluir(self):
+        print("telaIncluir")
+        self.botaoIncluir["state"] = "disabled"
+        self.botaoBuscar["state"] = "disabled"
+        self.labelTituloSecao = tk.Label(self.master,text="Incluir aluno",font="Arial 18")
+        self.labelTituloSecao.grid(row=0,column=1,padx=5,pady=5)
+        self.labelMatricula = tk.Label(self.master,text="Matrícula (somente números):",font="Arial 12")
+        self.labelMatricula.grid(row=1,column=1)
+        self.campoMatricula = tk.Entry(self.master)
+        self.campoMatricula.grid(row=1,column=2)
+        self.labelNome = tk.Label(self.master,text="Nome:",font="Arial 12")
+        self.labelNome.grid(row=2,column=1)
+        self.campoNome = tk.Entry(self.master)
+        self.campoNome.grid(row=2,column=2)
+        self.labelAV1 = tk.Label(self.master,text="Nota AV1:",font="Arial 12")
+        self.labelAV1.grid(row=3,column=1)
+        self.campoAV1 = tk.Entry(self.master)
+        self.campoAV1.grid(row=3,column=2)
+        self.labelAV2 = tk.Label(self.master,text="Nota AV2:",font="Arial 12")
+        self.labelAV2.grid(row=4,column=1)
+        self.campoAV2 = tk.Entry(self.master)
+        self.campoAV2.grid(row=4,column=2)
+        self.labelAV3 = tk.Label(self.master,text="Nota AV3:",font="Arial 12")
+        self.labelAV3.grid(row=5,column=1,padx=5,pady=5)
+        self.campoAV3 = tk.Entry(self.master)
+        self.campoAV3.grid(row=5,column=2,padx=5,pady=5)
+        self.labelAV4 = tk.Label(self.master,text="Nota AV4:",font="Arial 12")
+        self.labelAV4.grid(row=6,column=1,padx=5,pady=5)
+        self.campoAV4 = tk.Entry(self.master)
+        self.campoAV4.grid(row=6,column=2,padx=5,pady=5)
+        self.botaoGravar = tk.Button(self.master,text="Gravar",bg="green",fg="white",font="Arial 14",command=self.executaIncluirAluno)
+        self.botaoGravar.grid(row=7,column=1,ipadx=2,ipady=2,padx=10,pady=10)
+        self.botaoVoltar = tk.Button(self.master,text="Voltar",font="Arial 14 bold",command=self.executaVoltar)
+        self.botaoVoltar.grid(row=7,column=2,ipadx=2,ipady=2,padx=10,pady=20)
+
+    def executaIncluirAluno(self):
+        print("executaIncluirAluno")
+        self.retornoAluno = aluno.incluirAluno(self.idUsuario,self.campoNome.get(),self.campoMatricula.get(),self.campoAV1.get(),self.campoAV2.get(),self.campoAV3.get(),self.campoAV4.get())
+        if (self.retornoAluno == -1):
+            tk.messagebox.showinfo (title="Sucesso", message="Aluno incluído com sucesso.")
+            self.campoMatricula.delete(0, 'end')
+            self.campoNome.delete(0, 'end')
+            self.campoAV1.delete(0, 'end')
+            self.campoAV2.delete(0, 'end')
+            self.campoAV3.delete(0, 'end')
+            self.campoAV4.delete(0, 'end')
+        else:
+            print("Erro ",self.retornoAluno)
+            tk.messagebox.showerror(title="Erro", message="Erro ao incluir aluno.")
+            
+    def executaVoltar(self):
+        print("executaVoltar")
+        self.botaoIncluir["state"] = "normal"
+        self.botaoBuscar["state"] = "normal"
+        self.labelTituloSecao.destroy()
+        self.labelMatricula.destroy()
+        self.campoMatricula.destroy()
+        self.labelNome.destroy()
+        self.campoNome.destroy()
+        self.labelAV1.destroy()
+        self.campoAV1.destroy()
+        self.labelAV2.destroy()
+        self.campoAV2.destroy()
+        self.labelAV3.destroy()
+        self.campoAV3.destroy()
+        self.labelAV4.destroy()
+        self.campoAV4.destroy()
+        self.botaoGravar.destroy()
+        self.botaoVoltar.destroy()
+
     def sair(self):
         Principal.quit(self)
         print("Aplicativo encerrado.")
 usuario = Usuario()
 usuario.instalaBanco()
+aluno = Aluno()
 obj = Principal()
 obj.mainloop()
