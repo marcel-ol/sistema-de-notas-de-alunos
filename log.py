1b1c33dba029cb73c2f239c442598fcb432aaa9c
Marcel <marcel.ol@outlook.com>
2022-11-29 01:07:19 -0300

    correção final de nomes de arquivos

--- log.py
+++
@@ -1,7 +0,0 @@
-class log:
-    def novoLog():
-        conexao = pg.connect(host='127.0.0.1', database='postgres', user='postgres', password='1234')
-        novolog= "INSERT INTO log (matricula,login,motivo) VALUES (matricula,login,'Inclusão de nota')"
-        cursor=conexao.cursor()
-        cursor.execute (novolog)
-        print("Log com ação foi incluido")
